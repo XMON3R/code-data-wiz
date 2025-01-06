49676b18278a11bbc0b3c205a9f6e106422fdc3b
XMON3R <simon.juza@gmail.com>
2025-01-06 16:32:18 +0100

    deletion of uncessary commented code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,52 +1,8 @@
-/*import React, { useState } from 'react';
-//import '../styles/global.css';
-//import '../../build/css/global_new.css';
-import '../styles/translation.css';
-import useAppLogic from '../hooks/useAppLogic';
-import Navbar from './Navbar';
-import Editor from './Editor';
-import { VerticalSplitter } from '../utils/VerticalSplitter'; // Import the splitter component
-import { sql } from '@codemirror/lang-sql';
-
-import '../styles/output.css';
-
-const App: React.FC = () => {
-  const { textLeft, textRight, handleChangeTextLeft, sqlTranslation } = useAppLogic();
-  const [showSQL, setShowSQL] = useState(false);
-
-  //<Navbar showSQL={showSQL} onToggle={setShowSQL} />
-  return (
-    <div className="h-full bg-gray-900 text-white font-bold">
-      
-      <VerticalSplitter initialSize={50} className="translation flex h-full text-lg">
-        <div className="flex-1 h-full p-0">
-          <button>a</button>
-          <Editor
-            value={textLeft}
-            onChange={handleChangeTextLeft}
-            extensions={[]}
-           // className="flex-1 bg-gray-800 p-4"
-          />
-        </div>
-        <div className="flex-1 h-full p-0">
-        <button>a</button>
-          <Editor
-            value={showSQL ? sqlTranslation : textRight}
-            readOnly={true}
-            extensions={[sql()]}
-            // className="flex-1 bg-gray-800 p-4"
-          />
-        </div>
-      </VerticalSplitter>
-    </div>
-  );
-};
-*/
 import React, { useState } from 'react';
 import useAppLogic from '../hooks/useAppLogic';
 import Navbar from './Navbar';
 import Editor from './Editor';
-import { VerticalSplitter } from '../utils/VerticalSplitter'; // Import the splitter component
+import { VerticalSplitter } from '../utils/VerticalSplitter';
 import { sql } from '@codemirror/lang-sql';
 import '../styles/output.css';
 
@@ -64,7 +20,7 @@ const App: React.FC = () => {
           value={textLeft}
           onChange={handleChangeTextLeft}
           extensions={[]}
-          className="flex flex-col flex-grow h-screen bg-gray-900 text-white"
+          className="flex-col bg-gray-900 text-white"
         />
         <Editor
           value={showSQL ? sqlTranslation : textRight}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,62 +1,13 @@
 import React from 'react';
-import '../styles/index.css';
+//import '../styles/index.css';
 
 import '../styles/output.css';
 //import '../styles/navbar.css';
 
 interface NavbarProps {
   showSQL: boolean;
-  onToggle: (showSQL: boolean) => void; // Adjusted to accept a boolean argument
+  onToggle: (showSQL: boolean) => void; 
 }
-
-/*
-const Navbar: React.FC<NavbarProps> = ({ showSQL, onToggle }) => (
-  <div className="h-12 flex items-center">
-    <button
-      className={`nav-button ${!showSQL ? 'active' : ''}`}
-      onClick={() => onToggle(false)} // Pass false to show the class diagram
-    >
-      Class Diagram
-    </button>
-    <button
-      className={`nav-button ${showSQL ? 'active' : ''}`}
-      onClick={() => onToggle(true)} // Pass true to show the SQL query
-    >
-      SQL Query
-    </button>
-  </div>
-);
-
-export default Navbar;
-
-*/
-/*
-const Navbar: React.FC<NavbarProps> = ({ showSQL, onToggle }) => (
-  //<div className="h-12 flex items-center">
-  <div className="h-full border-2 border-gray-200">  
-    <button
-      className={`bg-gray-600 text-white px-4 py-2 m-0 border-none cursor-pointer transition-colors duration-300 ${
-        !showSQL ? 'bg-blue-600' : 'hover:bg-gray-700'
-      }`}
-      onClick={() => onToggle(false)} // Pass false to show the class diagram
-    >
-      Class Diagram
-    </button>
-    <button
-      className={`bg-gray-600 text-white px-4 py-2 m-0 border-none cursor-pointer transition-colors duration-300 ${
-        showSQL ? 'bg-blue-600' : 'hover:bg-gray-700'
-      }`}
-      onClick={() => onToggle(true)} // Pass true to show the SQL query
-    >
-      SQL Query
-    </button>
-  </div>
-);
-
-
-export default Navbar;
-*/
-
 
 const Navbar: React.FC<NavbarProps> = ({ showSQL, onToggle }) => (
   <div className="h-full border-2 border-gray-200 flex">

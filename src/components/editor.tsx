7eb070425162dbcc0e40f003cd4a1d3fd801c5fa
xmon3r <simon.juza@gmail.com>
2025-02-26 19:44:56 +0100

    add/new parser tests, still in making

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -12,26 +12,20 @@ interface EditorProps {
 }
 
 // Implementation of CodeMirror component for better visuals and formatting
-const Editor: React.FC<EditorProps> = ({
-  value,
-  onChange,
-  readOnly,
-  extensions,
-  className,
-}) => (
-  <div className={`h-full flex-1 grow-0 ${className}`}>
-    {' '}
-    {}
-    <CodeMirror
-      theme="dark"
-      value={value}
-      width="100%"
-      height={`${window.innerHeight}px`}
-      onChange={onChange}
-      readOnly={readOnly}
-      extensions={extensions}
-    />
-  </div>
-);
+function Editor({ value, onChange, readOnly, extensions, className }: EditorProps) {
+  return (
+    <div className={`h-full flex-1 grow-0 ${className}`}>
+      <CodeMirror
+        theme="dark"
+        value={value}
+        width="100%"
+        height={`${window.innerHeight}px`}
+        onChange={onChange}
+        readOnly={readOnly}
+        extensions={extensions}
+      />
+    </div>
+  );
+}
 
 export default Editor;
